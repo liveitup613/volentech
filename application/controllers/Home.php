73e937d34066bec3cd09b771b7bece8c7fa8d524
Imache Zerotech <88082376+EddyKelvin@users.noreply.github.com>
2022-02-25 19:03:35 +0000

    udpate

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -154,7 +154,7 @@ class Home extends CI_Controller {
 		$this->email->to('yugansh@volentech.com');
 		$this->email->subject('Contact Us');
 
-		$cotent = "Name: <strong>".$name. "</strong><br>Phone: <strong>" . $phone . "</strong><br>Email: <strong>" .$email ."</strong><br>Message:<br><strong>". $message.'</strong>';
+		$cotent = "Name: <strong>".$name. "</strong><br>Phone: <strong>" . $phone . "</strong><br>Email: <strong>" .$email ."</strong><br>Subject: <strong>" .$subject ."</strong><br>Message:<br><strong>". $message.'</strong>';
 		$this->email->message($cotent);
 
 		if ($this->email->send())
